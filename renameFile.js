09f71cbf433c4633ab908ee546c60eff7ed03a60
Alexey Melnikov <cancem343@gmail.com>
2023-05-06 12:05:30 +0300

    feat: add js file for task

--- renameFile.js
+++ renameFile.js
@@ -9,3 +9,12 @@ fs.rename(
     console.log('Файл переименован');
   }
 );
+
+// fs.writeFile(
+//   path.join(__dirname, 'notes', 'renameFile.js'),
+//   '',
+//   err => {
+//     if (err) throw err;
+//     console.log('file created')
+//   }
+// )
